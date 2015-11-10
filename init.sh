36208b94e7ff1d4a1f8a088521aa5b6083506896
Carlos Carcamo <carloscarcamo.m@gmail.com>
2015-11-10 11:15:33 -0600

    Algunas mejoras

--- init.sh
+++ init.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-echo "Descargando node app..."
+echo -e "[\e[5m....\e[25m] Descargando node app ..."
 git clone https://github.com/carloscarcamo/Nodejs-MongoDb-TodoMVC.git
 sleep 1
-echo "Renombrando directorio"
+echo -e "[\e[5m....\e[25m] Renombrando directorio ..."
 mv Nodejs-MongoDb-TodoMVC/ ./node/app/
+echo -e "[\e[5m....\e[25m] Configurando app.js ..."
+sed -i -e"s/8080/3000/" ./node/app/app.js
 sed -i -e"s/mongodb:\/\/localhost/mongodb:\/\/mongo/" ./node/app/app.js
-echo "node app lista"
+echo -e "[ \e[32mok\e[0m ] node app lista"
